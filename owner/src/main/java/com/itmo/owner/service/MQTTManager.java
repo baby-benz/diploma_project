ff11b82e693928bff9c1b51d9c8bc3692d0b2fe3
Nikita <baby0benz@gmail.com>
2021-08-31 15:24:12 +0300

    update MQTTManager to wait until pem file retrieving process will end

--- owner/src/main/java/com/itmo/owner/service/MQTTManager.java
+++ owner/src/main/java/com/itmo/owner/service/MQTTManager.java
@@ -1,60 +1,70 @@
 package com.itmo.owner.service;
 
+import com.itmo.owner.domain.entity.Equipment;
 import com.itmo.owner.domain.entity.Sensor;
-import com.itmo.owner.repository.SensorRepository;
+import com.itmo.owner.repository.EquipmentRepository;
 import com.itmo.owner.service.mqtt.SensorEmulator;
 import com.itmo.owner.service.mqtt.TopicReader;
 import lombok.RequiredArgsConstructor;
+import org.hyperledger.fabric.gateway.ContractException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 @Service
 @RequiredArgsConstructor
 public class MQTTManager {
-    private final SensorEmulator sensorEmulator;
-    private final SensorRepository sensorRepository;
+    private final TopicReader topicReader;
+    private final EquipmentRepository equipmentRepository;
 
     @Value("${mqtt.emulation}")
     private boolean toEmulateSensors;
 
+    @Transactional
     @EventListener
-    public void onApplicationLoaded(ContextRefreshedEvent event) {
-        List<Sensor> takenIntoAccountSensors = sensorRepository.findAllByEquipmentIsTakenIntoAccount(true);
+    public void onApplicationLoaded(ContextRefreshedEvent event) throws ContractException, IOException, InterruptedException, TimeoutException {
+        Thread.sleep(8000);
 
-        TopicReader.startRetrievingDataFromSensors(takenIntoAccountSensors);
+        List<Equipment> takenIntoAccountEquipment = equipmentRepository.findAllByIsTakenIntoAccountTrue();
 
-        if(toEmulateSensors) {
-            sensorEmulator.startEmulatingSensors(takenIntoAccountSensors);
+        if (toEmulateSensors) {
+            SensorEmulator.startEmulatingSensors(takenIntoAccountEquipment);
         }
+
+        topicReader.startRetrievingData(takenIntoAccountEquipment);
     }
 
-    public String subscribeOrUnsubscribe(@PathVariable Long sensorId, @RequestParam boolean toSubscribe) throws NoSuchFieldException {
-        Sensor sensorToSubscribe = sensorRepository.findById(sensorId).orElseThrow();
+    public String subscribeOrUnsubscribe(Long equipmentId, boolean toSubscribe) throws NoSuchFieldException, ContractException, IOException, InterruptedException, TimeoutException {
+        Equipment equipmentToSubscribe = equipmentRepository.findById(equipmentId).orElseThrow();
 
-        String topicName = sensorToSubscribe.getEquipment().getId() + "/" + sensorId;
-
-        if(toSubscribe) {
-            TopicReader.startRetrievingData(sensorToSubscribe);
-
-            if(toEmulateSensors) {
-                sensorEmulator.startEmulatingSensor(sensorToSubscribe);
+        if (toSubscribe) {
+            if (toEmulateSensors) {
+                //sensorEmulator.startEmulatingSensors(equipmentToSubscribe);
+                SensorEmulator.startEmulatingSensors(equipmentToSubscribe);
             }
 
-            return "Успешно оформлена подписка на топик " + topicName + ".";
+            topicReader.startRetrievingData(equipmentToSubscribe);
+
+            return "Успешно оформлена подписка на датчики оборудования с ID " + equipmentId;
         } else {
-            TopicReader.stopRetrievingData(topicName);
+            for (Sensor sensorToSubscribe : equipmentToSubscribe.getSensors()) {
+                String topicName = sensorToSubscribe.getEquipment().getId() + "/" + sensorToSubscribe.getId();
 
-            if(toEmulateSensors) {
-                sensorEmulator.stopEmulatingSensor(sensorToSubscribe);
+                topicReader.stopRetrievingData(topicName);
             }
 
-            return "Успешно прекращена подписка на топик " + topicName + ".";
+            if (toEmulateSensors) {
+                //sensorEmulator.stopEmulatingSensors(equipmentToSubscribe);
+                SensorEmulator.stopEmulatingSensors(equipmentToSubscribe);
+            }
+
+            return "Успешно прекращена подписка на датчики оборудования с ID " + equipmentId;
         }
     }
 }
