5aa334e80a257f0e80f89bec8e2a57508af62065
Nikita <baby0benz@gmail.com>
2021-08-31 14:25:29 +0300

    add converters to GensonConfig

---
+++ owner/src/main/java/com/itmo/owner/configuration/GensonConfig.java
@@ -0,0 +1,14 @@
+package com.itmo.owner.configuration;
+
+import com.owlike.genson.Genson;
+import com.owlike.genson.GensonBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GensonConfig {
+    @Bean
+    public Genson genson() {
+        return new GensonBuilder().withConverters(new LocalDateConverter(), new StringConverter()).create();
+    }
+}
