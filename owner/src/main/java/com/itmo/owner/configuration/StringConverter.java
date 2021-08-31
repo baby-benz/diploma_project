b31ee694407b3edc9f50df270c14507951c89030
Nikita <baby0benz@gmail.com>
2021-08-31 13:25:31 +0300

    add String converters for serializing/deserializing objects

---
+++ owner/src/main/java/com/itmo/owner/configuration/StringConverter.java
@@ -0,0 +1,24 @@
+package com.itmo.owner.configuration;
+
+import com.owlike.genson.Context;
+import com.owlike.genson.Converter;
+import com.owlike.genson.annotation.HandleNull;
+import com.owlike.genson.stream.ObjectReader;
+import com.owlike.genson.stream.ObjectWriter;
+
+@HandleNull
+public class StringConverter implements Converter<String> {
+    @Override
+    public void serialize(String string, ObjectWriter objectWriter, Context context) {
+        if(string == null) {
+            objectWriter.writeValue("");
+            return;
+        }
+        objectWriter.writeValue(string);
+    }
+
+    @Override
+    public String deserialize(ObjectReader objectReader, Context context) {
+        return objectReader.valueAsString();
+    }
+}
