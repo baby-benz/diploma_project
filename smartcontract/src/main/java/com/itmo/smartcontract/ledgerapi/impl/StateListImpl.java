d0d30ae0fd42be5f33c68125641c4503755dd1de
Nikita <baby0benz@gmail.com>
2021-08-31 14:27:54 +0300

    upgrade functionality and update contract of existing methods

--- smartcontract/src/main/java/com/itmo/smartcontract/ledgerapi/impl/StateListImpl.java
+++ smartcontract/src/main/java/com/itmo/smartcontract/ledgerapi/impl/StateListImpl.java
@@ -1,20 +1,23 @@
 package com.itmo.smartcontract.ledgerapi.impl;
 
+import com.itmo.smartcontract.entity.equipment.Equipment;
 import com.itmo.smartcontract.ledgerapi.State;
 import com.itmo.smartcontract.ledgerapi.StateDeserializer;
+import com.itmo.smartcontract.ledgerapi.StateHistory;
 import com.itmo.smartcontract.ledgerapi.StateList;
 import lombok.RequiredArgsConstructor;
-import org.hyperledger.fabric.Logger;
 import org.hyperledger.fabric.contract.Context;
 import org.hyperledger.fabric.shim.ChaincodeStub;
 import org.hyperledger.fabric.shim.ledger.CompositeKey;
+import org.hyperledger.fabric.shim.ledger.KeyModification;
+import org.hyperledger.fabric.shim.ledger.KeyValue;
+import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
 
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class StateListImpl implements StateList {
-    private final static Logger LOG = Logger.getLogger(StateListImpl.class.getName());
-
     private final Context ctx;
     private final String name;
     private final StateDeserializer deserializer;
@@ -25,11 +28,9 @@ public class StateListImpl implements StateList {
 
         CompositeKey ledgerKey = stub.createCompositeKey(this.name, state.getSplitKey());
 
-        byte[] data = State.serialize(state);
+        String data = State.GENSON.serialize(state);
 
-        LOG.info("Состояние при записи: " + new String(data, StandardCharsets.UTF_8));
-
-        stub.putState(ledgerKey.toString(), data);
+        stub.putStringState(ledgerKey.toString(), data);
 
         return this;
     }
@@ -40,7 +41,7 @@ public class StateListImpl implements StateList {
 
         CompositeKey ledgerKey = stub.createCompositeKey(this.name, State.splitKey(key));
 
-        byte[] data = stub.getState(ledgerKey.toString());
+        String data = stub.getStringState(ledgerKey.toString());
 
         if (data != null) {
             return this.deserializer.deserialize(data);
@@ -50,16 +51,65 @@ public class StateListImpl implements StateList {
     }
 
     @Override
+    public List<? extends State> getAllStates() {
+        ChaincodeStub stub = this.ctx.getStub();
+
+        QueryResultsIterator<KeyValue> allStates;
+
+        ArrayList<State> states = new ArrayList<>();
+
+        allStates = stub.getStateByPartialCompositeKey(stub.createCompositeKey(this.name));
+
+        for (KeyValue state : allStates) {
+            states.add(this.deserializer.deserialize(state.getStringValue()));
+        }
+
+        return states;
+    }
+
+    @Override
+    public List<StateHistory> getStateHistory(String key) {
+        ChaincodeStub stub = this.ctx.getStub();
+
+        CompositeKey ledgerKey = stub.createCompositeKey(this.name, State.splitKey(key));
+
+        QueryResultsIterator<KeyModification> stateHistory;
+
+        List<StateHistory> states = new ArrayList<>();
+
+        stateHistory = stub.getHistoryForKey(ledgerKey.toString());
+
+        stateHistory.forEach((state) -> states.add(
+                new StateHistory(
+                        state.getTxId(),
+                        (Equipment) this.deserializer.deserialize(state.getStringValue()),
+                        state.getTimestamp()
+                )
+        ));
+
+        return states;
+    }
+
+    @Override
     public StateList updateState(State state) {
         ChaincodeStub stub = this.ctx.getStub();
 
         CompositeKey ledgerKey = stub.createCompositeKey(this.name, state.getSplitKey());
 
-        byte[] data = State.serialize(state);
+        String data = State.GENSON.serialize(state);
 
-        LOG.info("Состояние при обновлении: " + new String(data, StandardCharsets.UTF_8));
+        stub.putStringState(ledgerKey.toString(), data);
 
-        stub.putState(ledgerKey.toString(), data);
+        return this;
+    }
+
+    @Override
+    public StateList deleteState(String key) {
+        ChaincodeStub stub = this.ctx.getStub();
+
+        CompositeKey ledgerKey = stub.createCompositeKey(this.name, State.splitKey(key));
+
+        stub.delState(ledgerKey.toString());
 
         return this;
     }
