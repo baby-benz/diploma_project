6dec07fd526264f1459009f90c35b74128fde85e
Nikita <baby0benz@gmail.com>
2021-08-31 14:32:32 +0300

    remove test DDO class and update API signature

--- owner/src/main/java/com/itmo/owner/controller/SubscriptionController.java
+++ owner/src/main/java/com/itmo/owner/controller/SubscriptionController.java
@@ -3,7 +3,11 @@ package com.itmo.owner.controller;
 import com.itmo.owner.service.MQTTManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hyperledger.fabric.gateway.ContractException;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @RestController
@@ -12,12 +16,8 @@ import org.springframework.web.bind.annotation.*;
 public class SubscriptionController {
     private final MQTTManager MQTTManager;
 
-    @PostMapping("/mqtt/sensor/{sensorId}")
-    public String subscribeOrUnsubscribe(@PathVariable Long sensorId, @RequestParam boolean toSubscribe) throws NoSuchFieldException {
-        return MQTTManager.subscribeOrUnsubscribe(sensorId, toSubscribe);
-    }
-
-    @PostMapping("/mqtt/hensor")
-    public void commission(@RequestBody DDO ddd) {
+    @PostMapping("/mqtt/equipment/{equipmentId}")
+    public String subscribeOrUnsubscribe(@PathVariable Long equipmentId, @RequestParam boolean toSubscribe) throws NoSuchFieldException, ContractException, IOException, InterruptedException, TimeoutException {
+        return MQTTManager.subscribeOrUnsubscribe(equipmentId, toSubscribe);
     }
 }
