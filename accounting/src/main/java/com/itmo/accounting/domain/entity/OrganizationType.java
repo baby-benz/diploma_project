9b31e5d0c8530748a0c36e5af9eeefe75ef9343a
Nikita <baby0benz@gmail.com>
2021-08-31 14:23:09 +0300

    add new organization type ("ЭКСПЛУАТАНТ")

--- accounting/src/main/java/com/itmo/accounting/domain/entity/OrganizationType.java
+++ accounting/src/main/java/com/itmo/accounting/domain/entity/OrganizationType.java
@@ -1,12 +1,13 @@
 package com.itmo.accounting.domain.entity;
 
 public enum OrganizationType {
-    ВЛАДЕЛЕЦ, НАДЗОР, СЕРВИС;
+    ВЛАДЕЛЕЦ, ЭКСПЛУАТАНТ, НАДЗОР, СЕРВИС;
 
     @Override
     public String toString() {
         switch(this) {
             case ВЛАДЕЛЕЦ: return "ВЛАДЕЛЕЦ";
+            case ЭКСПЛУАТАНТ: return "ЭКСПЛУАТАНТ";
             case НАДЗОР: return "НАДЗОР";
             case СЕРВИС: return "СЕРВИС";
             default: throw new IllegalArgumentException();
--- accounting/src/main/resources/init.sql
+++ accounting/src/main/resources/init.sql
@@ -1,6 +1,6 @@
 DROP TYPE IF EXISTS equipmenttype;;
 CREATE TYPE equipmenttype as ENUM ('ЦЕНТРОБЕЖНЫЙ_НАСОС','ПОРШНЕВОЙ_НАСОС','КОМПРЕССОР','ВИНТОВОЙ_ЗАБОЙНЫЙ_ДВИГАТЕЛЬ','СЕПАРАТОР','УЧАСТОК_ТРУБОПРОВОДА','РВС');;
 DROP TYPE IF EXISTS organizationtype;;
-CREATE TYPE organizationtype as ENUM ('ВЛАДЕЛЕЦ','НАДЗОР','СЕРВИС');;
+CREATE TYPE organizationtype as ENUM ('ВЛАДЕЛЕЦ','ЭКСПЛУАТАНТ','НАДЗОР','СЕРВИС');;
 DROP TYPE IF EXISTS sensortype;;
 CREATE TYPE sensortype as ENUM ('ДАТЧИК_ДАВЛЕНИЯ','ДАТЧИК_ТЕМПЕРАТУРЫ','ДАТЧИК_ОБОРОТОВ');;
