95fcc0d3a1a8bc29d1d6ff967bb351e61088c0d2
Nikita <baby0benz@gmail.com>
2021-08-31 14:28:51 +0300

    make TopicWriter protected and add logging to its method

--- owner/src/main/java/com/itmo/owner/service/mqtt/TopicWriter.java
+++ owner/src/main/java/com/itmo/owner/service/mqtt/TopicWriter.java
@@ -1,11 +1,15 @@
 package com.itmo.owner.service.mqtt;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import java.nio.ByteBuffer;
+
+@Slf4j
 @UtilityClass
-public class TopicWriter {
+class TopicWriter {
     public void publishMessage(String topicName, byte[] payload) throws MqttException {
         MqttMessage message = new MqttMessage(payload);
 
@@ -13,5 +17,6 @@ public class TopicWriter {
         message.setRetained(true);
 
         MQTTClient.MQTT_CLIENT.publish(topicName, message);
+        log.info("Опубликовано показание {} в топик {}", String.format("%.2f", ByteBuffer.wrap(payload).getDouble()), topicName);
     }
 }
