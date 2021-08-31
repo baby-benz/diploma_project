8a4c36f59ce77a29df014757c10040e19759860d
Nikita <baby0benz@gmail.com>
2021-08-31 14:27:45 +0300

    upgrade functionality and update contract of existing methods

--- smartcontract/src/main/java/com/itmo/smartcontract/ledgerapi/StateList.java
+++ smartcontract/src/main/java/com/itmo/smartcontract/ledgerapi/StateList.java
@@ -3,6 +3,8 @@ package com.itmo.smartcontract.ledgerapi;
 import com.itmo.smartcontract.ledgerapi.impl.StateListImpl;
 import org.hyperledger.fabric.contract.Context;
 
+import java.util.List;
+
 public interface StateList {
     static StateList getStateList(Context ctx, String listName, StateDeserializer deserializer) {
         return new StateListImpl(ctx, listName, deserializer);
@@ -10,7 +12,13 @@ public interface StateList {
 
     State getState(String key);
 
+    List<? extends State> getAllStates();
+
+    List<StateHistory> getStateHistory(String key);
+
     StateList addState(State state);
 
     StateList updateState(State state);
+
+    StateList deleteState(String key);
 }
