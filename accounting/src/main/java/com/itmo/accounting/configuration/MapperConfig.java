73c40b4545af620a962e0ed1f2193e94120e47c1
Nikita <baby0benz@gmail.com>
2021-08-31 14:51:57 +0300

    add converter to MapperConfig for avoiding null errors

--- accounting/src/main/java/com/itmo/accounting/configuration/MapperConfig.java
+++ accounting/src/main/java/com/itmo/accounting/configuration/MapperConfig.java
@@ -1,5 +1,9 @@
 package com.itmo.accounting.configuration;
 
+import com.itmo.accounting.controller.dto.NewEquipmentOrganizationDto;
+import com.itmo.accounting.domain.entity.EquipmentOrganization;
+import com.itmo.accounting.domain.entity.EquipmentOrganizationId;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +12,18 @@ import org.springframework.context.annotation.Configuration;
 public class MapperConfig {
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.createTypeMap(NewEquipmentOrganizationDto.class, EquipmentOrganization.class).setConverter(converter);
+        return modelMapper;
     }
+
+    Converter<NewEquipmentOrganizationDto, EquipmentOrganization> converter = context -> {
+        EquipmentOrganization equipmentOrganization = new EquipmentOrganization();
+        EquipmentOrganizationId id = new EquipmentOrganizationId();
+        id.setOrganizationId(context.getSource().getOrganizationId());
+        equipmentOrganization.setId(id);
+        equipmentOrganization.setStartDate(context.getSource().getStartDate());
+        equipmentOrganization.setEndDate(context.getSource().getEndDate());
+        return equipmentOrganization;
+    };
 }
